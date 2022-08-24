e2377af226b720250261cf71c44d816482083919
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-24 11:55:31 +0430

    Initial Repo

---
+++ StackWithLinkedList/StackWithLinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using StackWithLinkedList;
+
+var list = new Stack();
+
+list.Push(2);
+list.Pop();
+list.Pop();
+list.Push(4);
+list.Push(5);
+list.Push(6);
+
+list.Print();
+list.Pop();
+list.Pop();
+list.Pop();
+list.Pop();
+
+list.Push(5);
+list.Push(6);
+list.Push(6);
+list.Print();
---
+++ StackWithLinkedList/StackWithLinkedList/Stack.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackWithLinkedList
+{
+    public class Stack
+    {
+        public Node head;
+        public Node top;
+
+        public void Push(int value)
+        {
+            if (head == null)
+            {
+                Node newItem = new Node();
+                newItem.value = value;
+                head = newItem;
+                top = newItem;
+            }
+            else
+            {
+                Node newItem = new Node();
+                newItem.value = value;
+
+                Node node = head;
+                while (node.next != null)
+                {
+                    node = node.next;
+                }
+
+                node.next = newItem;
+                top = newItem;
+            }
+        }
+
+        public void Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The list is empty!");
+            }
+            else
+            {
+                Node node = head;
+
+                if (node.next != null)
+                {
+                    while (node.next.next != null)
+                    {
+                        node = node.next;
+                    }
+
+                    Console.WriteLine(top.value);
+                    top = null;
+                    top = node;
+                    top.next = null;
+                }
+                else
+                {
+                    Console.WriteLine(top.value);
+                    top = null;
+                    head = null;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The list is empty!");
+            }
+            else
+            {
+                Node node = head;
+                while (node != null)
+                {
+                    Console.WriteLine(node.value);
+                    node = node.next;
+                }
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (top == null || head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
